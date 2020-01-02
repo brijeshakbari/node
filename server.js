514197b62dddc067e3623bacda0b2e97ed8be082
nupur100 <37748784+nupur100@users.noreply.github.com>
2020-01-02 12:04:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Changed by Nupur-----Hello world today is last day of year \n');
+  res.send('Hellooo  Changed by Nupur-----Hello world today is last day of year \n');
 });
 
 app.listen(PORT, HOST);
